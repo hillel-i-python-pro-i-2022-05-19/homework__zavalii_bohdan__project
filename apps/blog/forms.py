20312755611157795fd5b9f3283552c64a89b256
econlq <zavaliybogdan@gmail.com>
2022-09-30 14:42:28 +0300

    ➕ Added simple CRUD functionality for Blogs

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,5 +1,17 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title", "slug", "author", "content")
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "slug": forms.TextInput(attrs={"class": "form-control"}),
+            "author": forms.Select(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control"}),
+        }
 
 
 class CommentForm(forms.ModelForm):
