2d347b8323908c0ad8f2e4169493b52763829273
econlq <zavaliybogdan@gmail.com>
2022-10-02 16:38:49 +0300

    🐛 Fixed 'Categories list' view and logic on blogs page

--- apps/blog/categories_views/categories_list.py
+++
@@ -1,8 +0,0 @@
-from django.views import generic
-
-from apps.blog.models import PostCategory
-
-
-class CategoriesList(generic.ListView):
-    model = PostCategory
-    template_name = "blogs/show_blogs.html"
