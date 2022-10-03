4bf6d35fdad0ef04c5ce237b83d3e6531f0a2989
econlq <zavaliybogdan@gmail.com>
2022-10-04 00:30:12 +0300

    💫 Updated PostDetail view logic

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,27 @@
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.views import generic
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
 from .models import Post, PostCategory
 from .forms import CommentForm, BlogForm, EditBlogForm
 from django.shortcuts import render, get_object_or_404
+
+
+def like_view(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get("post_id"))
+    liked = False  # flake8: noqa: F841
+
+    # If the user who clicked on 👍 already exists, then we remove him
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False  # flake8: noqa: F841
+    else:
+        # else we add/assign a like to the exact user
+        post.likes.add(request.user)
+        liked = True  # flake8: noqa: F841
+
+    return HttpResponseRedirect(reverse("blogs:post_detail", args=[str(post.slug)]))
 
 
 class PostList(generic.ListView):
@@ -30,7 +47,6 @@ class AddBlogView(CreateView):
 class UpdateBlogView(UpdateView):
     model = Post
     template_name = "blogs/update_post.html"
-    # fields = ["title", "slug", "blog_category", "content"]
     form_class = EditBlogForm
     success_url = reverse_lazy("blogs:show_all")
 
@@ -41,15 +57,41 @@ class DeleteBlogView(DeleteView):
     success_url = reverse_lazy("blogs:show_all")
 
 
-def post_detail(request, slug):
+class PostDetailView(DetailView):
     model = Post
     template_name = "blogs/post_detail.html"
-    post = get_object_or_404(model, slug=slug)
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    # Comment posted
-    if request.method == "POST":
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs["slug"]
+
+        # Comment form context data
+        comment_form = CommentForm()
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.filter(active=True)
+
+        # Likes context data
+        blog_id = get_object_or_404(Post, slug=slug)
+        total_likes = blog_id.total_likes()
+
+        liked = bool(blog_id.likes.filter(id=self.request.user.id).exists())
+
+        context["post"] = post
+        context["comments"] = comments
+        context["comment_form"] = comment_form
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context
+
+    def post(self, request, *args, **kwargs):
         comment_form = CommentForm(data=request.POST)
+        self.object = self.get_object()
+        slug = self.kwargs["slug"]
+        # context = super().get_context_data(**kwargs)
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.filter(active=True)
+        new_comment = None
+
         if comment_form.is_valid():
             # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
@@ -57,11 +99,10 @@ def post_detail(request, slug):
             new_comment.post = post
             # Save the comment to the database
             new_comment.save()
-    else:
-        comment_form = CommentForm()
-
-    return render(
-        request,
-        template_name,
-        {"post": post, "comments": comments, "new_comment": new_comment, "comment_form": comment_form},
-    )
+        else:
+            comment_form = CommentForm()
+        return render(
+            request,
+            self.template_name,
+            {"post": post, "comments": comments, "new_comment": new_comment, "comment_form": comment_form},
+        )
