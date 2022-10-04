5ce8b4d3e12b519740afed8f84789448aedb0b68
econlq <zavaliybogdan@gmail.com>
2022-10-04 23:13:17 +0300

    ➕ Added snippet for blog

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -9,25 +9,27 @@ CATEGORY_CHOICES_LIST = list(CATEGORY_CHOICES)
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title", "slug", "author", "blog_category", "content")
+        fields = ("title", "slug", "author", "blog_category", "content", "snippet")
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control"}),
             "slug": forms.TextInput(attrs={"class": "form-control"}),
             "author": forms.Select(attrs={"class": "form-control"}),
             "blog_category": forms.Select(choices=CATEGORY_CHOICES_LIST, attrs={"class": "form-control"}),
             "content": forms.Textarea(attrs={"class": "form-control"}),
+            "snippet": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 
 class EditBlogForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title", "slug", "blog_category", "content")
+        fields = ("title", "slug", "blog_category", "content", "snippet")
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control"}),
             "slug": forms.TextInput(attrs={"class": "form-control"}),
             "blog_category": forms.Select(choices=CATEGORY_CHOICES_LIST, attrs={"class": "form-control"}),
             "content": forms.Textarea(attrs={"class": "form-control"}),
+            "snippet": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 
